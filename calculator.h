93eace39a34cc0c6b4e6b10f65e810a272e8d603
michelle0yn <yena4150@naver.com>
2023-02-10 19:16:23 +0900

    first commit

---
+++ calculator.h
@@ -0,0 +1,16 @@
+class Calculator
+{
+public:
+    void input();
+    void calculate();
+    void output();
+private:
+    void add();
+    void sub();
+    void mul();
+    void divide();
+private:
+    int num1, num2;
+    char op;
+    float res;
+};
